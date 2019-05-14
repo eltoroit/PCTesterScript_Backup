aee72ab28f20bade1aa46b9ddfd189384d75c8da
Andres Perez <andresperezf@yahoo.com>
2019-05-14 13:59:48 -0300

    TEST

--- tester.js
+++ tester.js
@@ -14,7 +14,7 @@ const log = require('./colorLogs.js');
 
 // Configure execution...
 var timerDelay = 250;
-var testType = "PROD";
+var testType = "TEST";
 
 // Depending on execution (TEST | PROD)
 var debug = false;
